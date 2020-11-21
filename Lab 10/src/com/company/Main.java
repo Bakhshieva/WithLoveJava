d84e1d094024af2de76f850d8d657f554a3de29c
Bakhshieva <71921743+Bakhshieva@users.noreply.github.com>
2020-11-21 22:53:19 +0300

    Add files via upload

---
+++ Lab 10/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main
+{
+    public  static void main(String[] args)
+    {
+        Integer[] arrayOfStrings = {1, 2, 3, 4};
+
+        List<Integer> listOfStrings = new ArrayList<Integer>();
+        listOfStrings = Arrays.asList(arrayOfStrings);
+        System.out.println("Массив int");
+        for (Integer str : listOfStrings)
+            System.out.print(" " + str);
+
+        String[] arrayOfStrings1 = {"5", "6", "7", "8"};
+        List<String> listOfStrings1 = new ArrayList<String>();
+        listOfStrings1 = Arrays.asList(arrayOfStrings1);
+        System.out.println("\nМассив String");
+        for (String str : listOfStrings1)
+            System.out.print(" " + str);
+
+    }
+}
